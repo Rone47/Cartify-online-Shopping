5e0ab6c658dcd239ec932b84ed0a5d8258493a96
Rone47 <ryanbyrone47@gmail.com>
2025-06-09 20:52:34 +0300

    Integrate Clerk authentication by adding ClerkProvider to RootLayout, updating Header component for user management, and implementing middleware for Clerk

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,10 +1,11 @@
+import { SignInButton } from '@clerk/nextjs'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <button className='text-sm text-shop_dark_green font-semibold hover:text-shop_light_green hoverEffect cursor-pointer'>
+    <SignInButton className='text-sm text-shop_dark_green font-semibold hover:text-shop_light_green hoverEffect cursor-pointer'>
       Login
-    </button>
+    </SignInButton>
   )
 }
 
