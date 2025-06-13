b213842a1310849d6d2b25e8bbe7227b0e097088
Rone47 <ryanbyrone47@gmail.com>
2025-06-13 20:05:06 +0300

    Refactor SignIn component to use modal mode for SignInButton and improve button structure

--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -1,12 +1,14 @@
-import { SignInButton } from '@clerk/nextjs'
-import React from 'react'
+import { SignInButton } from "@clerk/nextjs";
+import React from "react";
 
 const SignIn = () => {
   return (
-    <SignInButton className='text-sm text-shop_dark_green font-semibold hover:text-shop_light_green hoverEffect cursor-pointer'>
-      Login
+    <SignInButton mode="modal">
+      <button className="text-sm text-shop_dark_green font-semibold hover:text-shop_light_green hoverEffect cursor-pointer">
+        Login
+      </button>
     </SignInButton>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
