be925d1029748d74be25183feafe557ae4fc8b95
Rone47 <ryanbyrone47@gmail.com>
2025-06-15 12:59:21 +0300

    Add global promo banner styles and update HomeBanner component for enhanced promotional display

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import { SubTitle, Title } from './ui/text'
+import React from "react";
+
 
 function HomeBanner() {
   return (
-    <div className='py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between'>
-      <div>
-        <Title>Enjoy Up to 50% Off on <br/> Selected Items  Selected  items</Title>
-        <SubTitle>Discover exclusive deals on a curated selection of <br/> products—limited-time offer while stocks last.</SubTitle>
+    <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
+      <div className="promo-banner">
+        <h1 className="promo-title">
+          Enjoy Up to <span className="highlight">50% Off</span> on <br />{" "}
+          Selected Items
+        </h1>
+        <h2 className="promo-subtitle">
+          Discover exclusive deals on a curated selection of products — <br />{" "}
+          limited-time offer while stocks last.
+        </h2>
       </div>
       <div></div>
     </div>
-  )
+  );
 }
 
-export default HomeBanner
+export default HomeBanner;
