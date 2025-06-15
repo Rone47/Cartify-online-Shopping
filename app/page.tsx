eb517698fa0937b009b057b8b174538f03a7338e
Rone47 <ryanbyrone47@gmail.com>
2025-06-15 13:46:07 +0300

    Update HomeBanner component with promotional link and adjust styles for better layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <Container className=" bg-shop_light_pink">
+    <Container className=" bg-shop_light_pink max-w-8xl mx-auto rounded-lg shadow-md">
       <HomeBanner />
     </Container>
   );
--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -1,11 +1,13 @@
+import { banner_1 } from "@/images";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
-
 
 function HomeBanner() {
   return (
-    <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
-      <div className="promo-banner">
-        <h1 className="promo-title">
+    <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between ">
+      <div className="promo-banner space-y-5">
+        <h1 className="promo-title font-sans mb-5">
           Enjoy Up to <span className="highlight">50% Off</span> on <br />{" "}
           Selected Items
         </h1>
@@ -13,8 +15,17 @@ function HomeBanner() {
           Discover exclusive deals on a curated selection of products — <br />{" "}
           limited-time offer while stocks last.
         </h2>
+        <Link
+          href={"/shop"}
+          className="bg-shop_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hoverEffect "
+        >
+          {" "}
+          Buy Now
+        </Link>
       </div>
-      <div></div>
+      <div >
+       <Image src={banner_1} alt='banner_1' className="hidden md:inline-flex w-96"/>
+      </div>
     </div>
   );
 }
