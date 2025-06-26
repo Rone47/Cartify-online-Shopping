9e1cbca9d4caa284670182ad4329c2016d0a42cb
Rone47 <ryanbyrone47@gmail.com>
2025-06-26 16:46:26 +0300

    feat: enhance ProductsGrid component with loading state and no product available message; add ProductCard and NoProductAvailable components

--- components/ProductsGrid.tsx
+++ components/ProductsGrid.tsx
@@ -1,13 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/constants/data";
 import { client } from "@/sanity/lib/client";
+import { AnimatePresence, motion } from "motion/react";
+import { Loader2 } from "lucide-react";
+import HomeTabbar from "./HomeTabBar";
+import NoProductAvailable from "./NoProductAvailable";
+import ProductCard from "./ProductCard";
 
 const ProductsGrid = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || '');
+  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
 
   const query = `*[_type == "product" && variant == $variant] | order(name asc){
   ...,"categories": categories[]->title
@@ -29,9 +33,32 @@ const ProductsGrid = () => {
     fetchData();
   }, [selectedTab]);
 
-  return <div className="">
-    <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab}/>
-  </div>;
+  return (
+    <div className="w-full">
+      <HomeTabbar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
+
+      {loading ? (
+        <div className="flex flex-col items-center justify-center py-10 min-h-80 space-y-4 text-center bg-gray-100 rounded-lg w-full mt-10">
+          <div className="space-x-2 flex text-center text-green-800">
+            <Loader2 className="w-5 h-6 animate-spin" />
+            <span>Product is Loading...</span>
+          </div>
+        </div>
+      ) : products?.length ? (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">
+          {products?.map((product) => (
+            <AnimatePresence key={product?._id}>
+              <motion.div>
+                <ProductCard product={product}/>
+              </motion.div>
+            </AnimatePresence>
+          ))}
+        </div>
+      ) : (
+        <NoProductAvailable selectedTab={selectedTab} />
+      )}
+    </div>
+  );
 };
 
 export default ProductsGrid;
