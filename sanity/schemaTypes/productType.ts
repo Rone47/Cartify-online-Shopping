f97982cc2dad9525c8409950f9d1c7c90b3fc228
Rone47 <ryanbyrone47@gmail.com>
2025-06-27 17:00:59 +0300

    fix: update discount field title to 'Discount Percentage %' for clarity

--- sanity/schemaTypes/productType.ts
+++ sanity/schemaTypes/productType.ts
@@ -49,7 +49,7 @@ export const productType = defineType({
     }),
     defineField({
       name: "discount",
-      title: "Discount",
+      title: "Discount Percentage %",
       type: "number",
       validation: (Rule) => Rule.required().min(0),
     }),
