70996cc0d76109ab2aff690603e5eca5ad15a302
Rone47 <ryanbyrone47@gmail.com>
2025-06-30 21:16:22 +0300

    feat: enhance HomeCategories component with additional styling; add getCategories query for fetching categories

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -4,7 +4,7 @@ import Title from './Title'
 const HomeCategories = () => {
   return (
     <div className='bg-white border border-shop_light_green/20 my-10 md:my-20 p-5 lg:p-7 rounded-md'>
-         <Title>Popular Categories</Title>
+         <Title className='border-b pb-3'>Popular Categories</Title>
     </div>
    
   )
