817c7d7acb57dc7fa9c7350e35a2966e2b493beb
Rone47 <ryanbyrone47@gmail.com>
2025-07-11 18:52:15 +0300

    feat: implement ImageView component with image selection and animation

--- components/ImageView.tsx
+++ components/ImageView.tsx
@@ -4,7 +4,10 @@ import {
   SanityImageCrop,
   SanityImageHotspot,
 } from "@/sanity.types";
-import React from 'react';
+import { urlFor } from "@/sanity/lib/image";
+import { AnimatePresence, motion } from "motion/react";
+import Image from "next/image";
+import React, { useState } from "react";
 
 interface Props {
   images?: Array<{
@@ -22,10 +25,53 @@ interface Props {
   isStock?: number | undefined;
 }
 
-const ImageView = () => {
-  return (
-    <div>ImageView</div>
-  )
-}
+const ImageView = ({ images = [], isStock }: Props) => {
+  const [active, setActive] = useState(images[0]);
+  console.log(active);
 
-export default ImageView
+  return (
+    <div className="w-full md:w-1/2 space-y-2 md:space-y-4">
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={active?._key}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="w-full max-h-[550px] min-h-[450px] border border-darkColor/10 rounded-md group overflow-hidden"
+        >
+          <Image
+            src={urlFor(active).url()}
+            alt="productImage"
+            width={700}
+            height={700}
+            priority
+            className={`w-full h-96 max-h-[550px] min-h-[500px] object-contain group-hover:scale-110 hoverEffect rounded-md ${
+              isStock === 0 ? "opacity-50" : ""
+            }`}
+          />
+        </motion.div>
+      </AnimatePresence>
+      <div className="grid grid-cols-6 gap-2 h-20 md:h-24">
+        {images?.map((image) => (
+          <button
+            key={image?._key}
+            onClick={() => setActive(image)}
+            title={`Select image ${image._key}`}
+            className={`border rounded-md overflow-hidden ${active?._key === image?._key ? "border-darkColor opacity-100" : "opacity-80"}`}
+          >
+            <Image
+              src={urlFor(image).url()}
+              alt={`Thumbnail ${image._key}`}
+              width={100}
+              height={100}
+              className="w-full h-auto object-contain"
+            />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageView;
