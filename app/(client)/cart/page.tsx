429644c11d57ea1c5b86b6672264371f902a43f7
Rone47 <ryanbyrone47@gmail.com>
2025-07-21 21:05:15 +0300

    Add

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -1,11 +1,39 @@
-'use client';
+"use client";
 
-import React from 'react';
+import Container from "@/components/Container";
+import NoAccess from "@/components/NoAccess";
+import useStore from "@/store";
+import { useAuth, useUser } from "@clerk/nextjs";
+import React, { useState } from "react";
 
 const Cartpage = () => {
-  return (
-    <div>Cartpage</div>
-  )
-}
 
-export default Cartpage
+  const {
+    deleteCartProduct,
+    getTotalPrice,
+    getItemCount,
+    getSubTotalPrice,
+    resetCart,
+  } = useStore();
+  const [loading, setLoading] = useState(false);
+  //   const groupedItems = useStore((state) => state.getGroupedItems());
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+  //   const [addresses, setAddresses] = useState<Address[] | null>(null);
+  const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
+  
+
+  return (
+    <div className="bg-gray-50 pb-52 md:pb-10">
+      {isSignedIn ? (
+        <Container>
+          <p>Cart Page</p>
+        </Container>
+      ) : (
+        <NoAccess />
+      )}
+    </div>
+  );
+};
+
+export default Cartpage;
